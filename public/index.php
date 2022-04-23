d0b8f292ce06dfb64f557f52ab15179bd4cd4bdc
karim ali hussein <karimalihussein99@gmail.com>
2022-04-23 22:06:12 +0200

    add all theose examples in tests file you can test it in unit test

--- public/index.php
+++ public/index.php
@@ -7,18 +7,6 @@ require_once __DIR__ . '/../vendor/autoload.php';
 
 use App\SelectionSort\SelectionSort;
 
-$array = [-1,30, 3, 6, -10 , 0,2,120, 10, 60];
-// echo $array[2];die;
-$sort = new SelectionSort($array);
- var_dump($sort->sort());
-// echo $array[$sort->findBiggest()];
-// echo $array[$sort->findBiggest()];
-// var_dump($sort->findSmaill());
-
-// var_dump($sort->sort());
-
-
-//  var_dump($sort->sort());
 
 
 
